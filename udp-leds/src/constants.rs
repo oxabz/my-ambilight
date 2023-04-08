8dede3c991570f2537c6f560488e508e631c5cb2
oxabz <legmatt0@gmail.com>
2023-04-08 13:48:01 +0200

    Adding a port constant in the protocol

--- udp-leds/src/constants.rs
+++ udp-leds/src/constants.rs
@@ -1,5 +1,6 @@
 pub const MAX_LED_COUNT: usize = 256;
 pub const MAX_MESSAGE_LENGTH: usize = MAX_LED_COUNT * 3 + 2;
+pub const PORT: u16 = 52772;
 
 pub(crate) const SERVER_FLAG: u8 = 0b1110_0110;
 pub(crate) const CLIENT_FLAG: u8 = 0b0110_1011;
